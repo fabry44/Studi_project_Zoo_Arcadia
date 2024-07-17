719bdf514555e283d721ef2dbe61f3df62ee65f4
fabryq <120800630+fabryq@users.noreply.github.com>
2024-07-17 03:29:30 +0200

    Implement the template habitat with Bootstrap html/css with js for switch habitats. Design in firstmobile but not actually responsive

--- assets/app.js
+++ assets/app.js
@@ -7,9 +7,10 @@ import './bootstrap.js';
  */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
-
-
-
+import './styles/menu.css';
+import './styles/footer.css';
+import './styles/acceuil.css';
+import './styles/habitats.css';
 
 
 /*
@@ -23,5 +24,34 @@ window.bootstrap = bootstrap;
 
 
 
-
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('.container-link');
+    const sections = document.querySelectorAll('.habitat_section');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault(); // Prevent the default link behavior
+            const habitatName = this.getAttribute('data-habitat');
+
+            // Remove 'active' class and add 'inactive' class to all links
+            links.forEach(lnk => {
+                lnk.classList.remove('active');
+                lnk.classList.add('inactive');
+            });
+
+            // Add 'active' class and remove 'inactive' class to the clicked link
+            this.classList.add('active');
+            this.classList.remove('inactive');
+
+            // Hide all sections
+            sections.forEach(section => {
+                section.style.display = 'none';
+            });
+
+            // Show the selected section
+            document.getElementById(habitatName).style.display = 'block';
+        });
+    });
+});
