59f66a2458b45b2d6a25179fdaecccfb7aaf2e64
fabryq <120800630+fabryq@users.noreply.github.com>
2024-07-22 13:37:43 +0200

    Configured environment variables for Heroku

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -8,10 +8,15 @@ if (file_exists(dirname(__DIR__).'/.env')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
 
-$dsn = getenv('DATABASE_URL');
-if ($dsn !== false) {
-    $parsedUrl = parse_url($dsn);
+$postgresDsn = getenv('DATABASE_URL');
+if ($postgresDsn !== false) {
+    $parsedUrl = parse_url($postgresDsn);
     putenv('DATABASE_DSN=pgsql:host=' . $parsedUrl['host'] . ';port=' . $parsedUrl['port']);
     putenv('DATABASE_USER=' . $parsedUrl['user']);
     putenv('DATABASE_PASSWORD=' . $parsedUrl['pass']);
+}
+
+$mongoDsn = getenv('MONGODB_URI');
+if ($mongoDsn !== false) {
+    putenv('MONGO_DSN=' . $mongoDsn);
 }
