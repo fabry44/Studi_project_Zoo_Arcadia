ca28104554846f05e666ec3b1ee12ad85bb03ddc
fabryq <120800630+fabryq@users.noreply.github.com>
2024-07-22 13:43:36 +0200

    Remove .env file and update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,17 +5,19 @@ use Symfony\Component\Dotenv\Dotenv;
 require dirname(__DIR__).'/vendor/autoload.php';
 
 if (file_exists(dirname(__DIR__).'/.env')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/'.'.env');
 }
 
-$postgresDsn = getenv('DATABASE_URL');
-if ($postgresDsn !== false) {
-    $parsedUrl = parse_url($postgresDsn);
+// PostgreSQL configuration
+$dsn = getenv('DATABASE_URL');
+if ($dsn !== false) {
+    $parsedUrl = parse_url($dsn);
     putenv('DATABASE_DSN=pgsql:host=' . $parsedUrl['host'] . ';port=' . $parsedUrl['port']);
     putenv('DATABASE_USER=' . $parsedUrl['user']);
     putenv('DATABASE_PASSWORD=' . $parsedUrl['pass']);
 }
 
+// MongoDB configuration
 $mongoDsn = getenv('MONGODB_URI');
 if ($mongoDsn !== false) {
     putenv('MONGO_DSN=' . $mongoDsn);
