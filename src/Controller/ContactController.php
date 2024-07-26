d50f044f3f3b99a21cf6645a0229d707df0f6be6
fabryq <120800630+fabryq@users.noreply.github.com>
2024-07-26 02:53:05 +0200

    Mailer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use Mailgun\Mailgun;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,17 +22,34 @@ class ContactController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
 
-            // Envoi de l'email au zoo
-            $email = (new Email())
-                ->from($data['email'])
-                ->to('contact@zoo-arcadia.com') // Remplacez par l'adresse email du zoo
-                ->subject($data['title'])
-                ->text($data['description']);
+             // Utiliser Mailgun pour envoyer l'email
+            $mgClient = Mailgun::create($_ENV['MAILGUN_API_KEY']);
+            $domain = $_ENV['MAILGUN_DOMAIN'];
+            $params = [
+                'from'    => $_ENV['contact@arcadia.com'],
+                'to'      => $_ENV['fabienroy2@gmail.com'],
+                'subject' => $data['title'],
+                'text'    => $data['description']
+            ];
 
-            $mailer->send($email);
+            try {
+                $mgClient->messages()->send($domain, $params);
+                $this->addFlash('success', 'Votre demande a été envoyée avec succès.');
+            } catch (\Exception $e) {
+                $this->addFlash('error', 'Une erreur est survenue lors de l\'envoi de votre demande.');
+            }
+
+            // Envoi de l'email au zoo
+            // $email = (new Email())
+            //     ->from($data['email'])
+            //     ->to('fabienroy2@gmail.com') // Remplacez par l'adresse email du zoo
+            //     ->subject($data['title'])
+            //     ->text($data['description']);
+
+            // $mailer->send($email);
 
             // Ajouter un message flash
-            $this->addFlash('success', 'Votre demande a été envoyée avec succès.');
+            // $this->addFlash('success', 'Votre demande a été envoyée avec succès.');
 
             return $this->redirectToRoute('app_contact');
         }
