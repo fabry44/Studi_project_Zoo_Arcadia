dec1a581a4262b5d458d4c5144741690a3ed0bfc
fabryq <120800630+fabryq@users.noreply.github.com>
2024-07-27 01:45:46 +0200

    easyadmin

--- src/Controller/Admin/Crud/HoraireCrudController.php
+++ src/Controller/Admin/Crud/HoraireCrudController.php
@@ -8,7 +8,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -18,29 +18,28 @@ class ExceptionListener
 
     public function onKernelException(ExceptionEvent $event)
     {
-        // You get the exception object from the received event
-        $exception = $event->getThrowable();
-        $statusCode = $exception instanceof HttpExceptionInterface ? $exception->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
-
-        // Customize your response object to display the exception details
-        $response = new Response();
         
-        if ($statusCode === 404) {
-            $response->setContent($this->twig->render('error/404.html.twig'));
-        } else {
-            $response->setContent($this->twig->render('error/500.html.twig', ['message' => $exception->getMessage()]));
-        }
+        // $exception = $event->getThrowable();
+        // $statusCode = $exception instanceof HttpExceptionInterface ? $exception->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
 
-        // HttpExceptionInterface is a special type of exception that
-        // holds status code and header details
-        if ($exception instanceof HttpExceptionInterface) {
-            $response->setStatusCode($exception->getStatusCode());
-            $response->headers->replace($exception->getHeaders());
-        } else {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+        
+        // $response = new Response();
+        
+        // if ($statusCode === 404) {
+        //     $response->setContent($this->twig->render('error/404.html.twig'));
+        // } else {
+        //     $response->setContent($this->twig->render('error/500.html.twig', ['message' => $exception->getMessage()]));
+        // }
 
-        // Send the modified response object to the event
-        $event->setResponse($response);
+        
+        // if ($exception instanceof HttpExceptionInterface) {
+        //     $response->setStatusCode($exception->getStatusCode());
+        //     $response->headers->replace($exception->getHeaders());
+        // } else {
+        //     $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        // }
+
+        
+        // $event->setResponse($response);
     }
 }
